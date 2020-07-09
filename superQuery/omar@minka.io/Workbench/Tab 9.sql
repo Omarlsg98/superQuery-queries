7b520e14a1bc56043a0149b1b0ff43a9d6a783d9
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-09 10:16:55 -0500

    Auto-commit: Thu Jul 09 2020 10:16:40 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 9.sql
+++ superQuery/omar@minka.io/Workbench/Tab 9.sql
@@ -1,25 +1,4 @@
-CREATE TABLE minka-ach-dw.ach_tin_before.transfer_20200701_1415 AS
 SELECT 
-  *
-FROM 
-    minka-ach-dw.ach_tin_20200701_1415.transfer;
-CREATE TABLE minka-ach-dw.ach_tin_before.transfer_20200702_1159 AS
-SELECT 
-  *
-FROM 
-    minka-ach-dw.ach_tin_20200702_1159.transfer;
-CREATE TABLE minka-ach-dw.ach_tin_before.transfer_20200703_2000 AS
-SELECT 
-  *
-FROM 
-    minka-ach-dw.ach_tin_20200703_2000.transfer;
-CREATE TABLE minka-ach-dw.ach_tin_before.transfer_20200704_1630 AS
-SELECT 
-  *
-FROM 
-    minka-ach-dw.ach_tin_20200704_1630.transfer;
-CREATE TABLE minka-ach-dw.ach_tin_before.transfer_20200705_0800 AS
-SELECT 
-  *
-FROM 
-    minka-ach-dw.ach_tin_20200705_0800.transfer;
+ * 
+FROM
+    minka-ach-dw.ach_tin.transfer
