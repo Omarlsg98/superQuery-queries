506313eea56992f225f53185fc019a39740acd58
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-09 11:47:03 -0500

    Auto-commit: Thu Jul 09 2020 11:46:56 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Scenario_finder/transfiya_pivot_specific.sql
+++ superQuery/omar@minka.io/Scenario_finder/transfiya_pivot_specific.sql
@@ -33,7 +33,7 @@ FROM
     minka-ach-dw.temp.tx_n_actions
 /*---WHERE---*/
 WHERE 
-   created> "2020-07-09T23:00" 
+   created> "2020-07-08T23:00" 
 GROUP BY
     status, upload_,main_action_,download_target_,reject_,download_source_,download_ambiguous_,source_bank,target_bank
 ORDER BY
