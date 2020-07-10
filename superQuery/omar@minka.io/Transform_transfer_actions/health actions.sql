1fb8b677aa4584b99b2929a6adf16f3b050f8df5
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 15:49:44 -0500

    Auto-commit: Fri Jul 10 2020 15:49:37 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_transfer_actions/health actions.sql
+++ superQuery/omar@minka.io/Transform_transfer_actions/health actions.sql
@@ -6,17 +6,18 @@ SELECT
     + SUM(download_ambiguous.count)
     + SUM(reject.count) as total_actions
     , "tx_n_actions" as descr
+    ,MAX(created)
 FROM 
     minka-ach-dw.temp.tx_n_actions
 UNION ALL
 SELECT
-    COUNT(action_id)-2479, "action" as descr
+    COUNT(action_id)-2479, "action" as descr,MAX(action_created)
 FROM
     minka-ach-dw.ach_tin.action
 WHERE 
     action_type not in ("ISSUE", "TOPUP", "WITHDRAW")
 UNION ALL
 SELECT
-    COUNT(action_id), "action_new_downloads"
+    COUNT(action_id), "action_new_downloads",MAX(action_created)
 FROM 
     minka-ach-dw.temp.action_new_downloads
