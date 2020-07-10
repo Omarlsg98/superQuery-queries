7ce6b71c15f6e02eaf07c306534534e86ef2034d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 10:46:09 -0500

    Auto-commit: Fri Jul 10 2020 10:46:01 GMT-0500 (hora estándar de Colombia)

---
+++ superQuery/omar@minka.io/Workbench/movii_analisis.sql
@@ -0,0 +1,28 @@
+WITH 
+movii_types AS 
+(    SELECT 
+        SUBSTR(TRANSFER_ID,1,2) AS type
+        ,CELL_ID AS transfer_id
+        ,ERROR_CODE AS amount
+    FROM
+        `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
+)
+,movii_balance AS 
+(
+SELECT 
+    transfer_id
+    , COUNTIF(type="CI") AS cashins
+    , COUNTIF(type="MP") AS cashouts
+    , SUM(IF(type="CI",amount,-amount)) AS balance
+    , MIN(amount)-MAX(amount) AS healthcheck
+FROM
+    movii_types
+GROUP BY
+    transfer_id
+)
+SELECT
+    *
+FROM
+    movii_balance
+WHERE
+    healthcheck!=0
