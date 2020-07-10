0c00afd3c1b20ad24af5726cce8d702804ff5af2
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 09:37:20 -0500

    Auto-commit: Fri Jul 10 2020 09:37:14 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Scenario_finder/Tab 8.sql
+++ superQuery/omar@minka.io/Scenario_finder/Tab 8.sql
@@ -1,4 +1,4 @@
-SELECT CAST(SUBSTR(created,1,19) AS DATETIME),created
+SELECT created ,CAST(SUBSTR(created,1,19) AS DATETIME), DATETIME_SUB(CURRENT_DATETIME("America/Bogota"),INTERVAL 1 DAY)
 FROM 
     minka-ach-dw.ach_tin.transfer
 LIMIT 10
