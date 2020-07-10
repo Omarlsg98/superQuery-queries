5a29a63801c832badbf4670a2e6ce381d9e6a9c9
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 09:28:05 -0500

    Auto-commit: Fri Jul 10 2020 09:27:58 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Scenario_finder/Tab 8.sql
+++ superQuery/omar@minka.io/Scenario_finder/Tab 8.sql
@@ -1,3 +1,4 @@
-SELECT wallet_id, handle, signer, wallet_default, type, created, name, wallet_phone
-FROM minka-ach-dw.ach_tin.wallet
-WHERE handle = "$573008333179"
+SELECT CAST(SUBSTR(created,1,19) AS DATETIME)
+FROM 
+    minka-ach-dw.ach_tin.transfer
+LIMIT 10
