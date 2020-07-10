86ca12508cb612ae3efd44be7160afa421e073dd
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 10:50:52 -0500

    Auto-commit: Fri Jul 10 2020 10:50:45 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 10.sql
+++ superQuery/omar@minka.io/Workbench/Tab 10.sql
@@ -1,6 +1,7 @@
  SELECT 
-       count(*)
+        string_field_7
+      , count(*)
 FROM
     `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
-WHERE
-    string_field_7="TD"
+GROUP BY
+    string_field_7
