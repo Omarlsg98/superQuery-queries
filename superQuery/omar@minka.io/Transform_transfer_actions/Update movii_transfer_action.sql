5c1dbff4b77e6b67a0243ea13e1bb6dc97086251
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 17:56:45 -0500

    Auto-commit: Fri Jul 10 2020 17:56:36 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_transfer_actions/Update movii_transfer_action.sql
+++ superQuery/omar@minka.io/Transform_transfer_actions/Update movii_transfer_action.sql
@@ -8,7 +8,9 @@ SELECT
   , action.error_code AS action_error_code
   , action.error_message AS action_error_message
 FROM
-    `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09` AS movii
-INNER JOIN   minka-ach-dw.ach_tin.transfer_action ON UPPER(transfer.transfer_id)=movii.cell_id
+  minka-ach-dw.ach_tin.transfer
+LEFT JOIN 
+    `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09` AS movii ON UPPER(transfer.transfer_id)=movii.cell_id
+LEFT JOIN   minka-ach-dw.ach_tin.action ON transfer.transfer_id = action.transfer_id
 WHERE 
     transfer.transfer_id="9p6hhE32ZvakHTCHi"
