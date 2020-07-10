a142eee5d05f37961dcefe4501b1d3daa65d0299
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 10:34:43 -0500

    Auto-commit: Fri Jul 10 2020 10:34:35 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 11.sql
+++ superQuery/omar@minka.io/Workbench/Tab 11.sql
@@ -1,7 +1,6 @@
 SELECT 
-    SUBSTR(TRANSFER_ID,1,3) AS type
-    , COUNT(CELL_ID)
+    #SUBSTR(TRANSFER_ID,1,2) AS type
+    MIN(TRANSFER_ON),
+    MAX(TRANSFER_ON)
 FROM
     `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
-GROUP BY
-    type
