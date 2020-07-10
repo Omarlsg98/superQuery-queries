8d57c3ff5c043f41977adcea94bd492b8eddbff4
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 16:02:42 -0500

    Auto-commit: Fri Jul 10 2020 16:02:32 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_analisis.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_analisis.sql
@@ -1,13 +1,13 @@
 WITH 
 movii_types AS 
 (    SELECT 
-        SUBSTR(TRANSFER_ID,1,2) AS type
-        ,CELL_ID AS transfer_id
-        ,ERROR_CODE AS amount
+        SUBSTR(movii_transfer_id,1,2) AS type
+        ,transfer_id
+        ,amount
     FROM
-        `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
+        minka-ach-dw.movii_bridge_log.movii_logs_20_07_09
     WHERE 
-        string_field_7="TS"
+        transfer_status="TS"
 )
 ,movii_balance AS 
 (
@@ -15,7 +15,7 @@ SELECT
     transfer_id
     , COUNTIF(type="CI") AS cashins
     , COUNTIF(type="MP") AS cashouts
-    , SUM(IF(type="CI",amount,-amount)) AS balance
+    , SUM(IF(type="CI",amount,-amount)) AS movii_balance
     , MIN(amount)-MAX(amount) AS healthcheck
 FROM
     movii_types
@@ -28,12 +28,12 @@ SELECT
     transfer_id
     , COUNTIF(action_type="DOWNLOAD") AS downloads
     , COUNTIF(action_type="UPLOAD") AS uploads
-    , SUM(IF(action_type="DOWNLOAD",-CAST(action_amount AS FLOAT64),CAST(action_amount AS FLOAT64))) AS balance
+    , SUM(IF(action_type="DOWNLOAD",-CAST(action_amount AS FLOAT64),CAST(action_amount AS FLOAT64))) AS action_balance
 FROM
     minka-ach-dw.ach_tin.action
 WHERE
     action_type IN ("DOWNLOAD", "UPLOAD")
-    AND action_hash!="PENDING"
+    AND action_hash NOT IN ("PENDING")
     AND (action_source_signer="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2" 
         OR action_target_signer="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2")
 GROUP BY
@@ -44,14 +44,14 @@ GROUP BY
 SELECT 
     movii.transfer_id AS movii_transfer_id
     ,action.transfer_id AS action_transfer_id
-    ,movii.balance AS movii_balance
-    ,action.balance AS action_balance
-    ,(movii.balance + action.balance) AS match
+    ,movii_balance
+    ,action_balance
+    ,(movii_balance + action_balance) AS match
 FROM
     movii_balance AS movii
 LEFT JOIN
     action_balance AS action
-        ON UPPER(action.transfer_id)= movii.transfer_id
+        ON action.transfer_id= movii.transfer_id
 WHERE
     action.transfer_id IS NOT NULL
 )
@@ -60,5 +60,3 @@ SELECT
     ,COUNTIF(match!=0) as no_match
 FROM
     match_table
-WHERE
-    match!=0
