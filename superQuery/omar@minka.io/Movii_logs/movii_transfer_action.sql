4516eee3f7d1bab9a85667ac612b12bb54f22ea0
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 15:55:07 -0500

    movii_logs1

---
+++ superQuery/omar@minka.io/Movii_logs/movii_transfer_action.sql
@@ -0,0 +1,14 @@
+SELECT 
+    movii.transfer_id
+  , SUBSTR(movii.transfer_id,1,2) AS movii_type
+  ,* EXCEPT(transfer_id,amount, error_code, error_message)
+  , transfer.amount AS amount
+  , transfer.error_code AS transfer_error_code
+  , transfer.error_message AS transfer_error_message
+  , action.error_code AS action_error_code
+  , action.error_message AS action_error_message
+FROM
+    `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09` AS movii
+LEFT JOIN   minka-ach-dw.ach_tin.transfer ON UPPER(transfer.transfer_id)=movii.cell_id
+LEFT JOIN   minka-ach-dw.ach_tin.action ON movii.cell_id = UPPER(action.transfer_id)
+LIMIT 10
