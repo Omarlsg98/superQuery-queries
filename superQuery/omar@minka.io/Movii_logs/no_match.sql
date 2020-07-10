d7548c3e0d5982258d4c70c394a6c8ce5f501ce6
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 16:54:30 -0500

    Auto-commit: Fri Jul 10 2020 16:54:24 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/no_match.sql
@@ -5,5 +5,5 @@ FROM
     minka-ach-dw.movii_bridge_log.movii_action_no_match_20_07_08 AS no_match
 LEFT JOIN 
     minka-ach-dw.ach_tin.transfer
-     ON transfer.trasnsfer_id=no_match.transfer_id
+     ON transfer.transfer_id=no_match.transfer_id
 LIMIT 10
