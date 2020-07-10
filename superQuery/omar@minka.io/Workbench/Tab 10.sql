af5f6dcf88d99522dae44e5f6e4f01e8f910edb4
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 12:48:17 -0500

    Auto-commit: Fri Jul 10 2020 12:48:04 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 10.sql
+++ superQuery/omar@minka.io/Workbench/Tab 10.sql
@@ -1,10 +1,13 @@
- CREATE TABLE minka-ach-dw.movii_bridge_log.movii_logs_20_07_09_gc AS
- SELECT 
-     CELL_ID AS cell_id
-     ,TRANSFER_ID AS transfer_id
-     ,MSISDN AS cellphone
-     ,ERROR_CODE AS amount
-     ,TRANSFER_STATUS AS error_code
-     ,string_field_7 AS transfer_status
+SELECT 
+  * EXCEPT(transfer_id, amount, error_code, error_message)
+  , transfer.transfer_id as transfer
+  , transfer.amount AS amount
+  , transfer.error_code AS transfer_error_code
+  , transfer.error_message AS transfer_error_message
+  , action.error_code AS action_error_code
+  , action.error_message AS action_error_message
 FROM
-    `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
+    `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09_gc` AS movii
+LEFT JOIN   minka-ach-dw.ach_tin.transfer ON UPPER(transfer.transfer_id)=movii.transfer_id
+LEFT JOIN   minka-ach-dw.ach_tin.action ON movii.transfer_id = UPPER(action.transfer_id)
+LIMIT 10
