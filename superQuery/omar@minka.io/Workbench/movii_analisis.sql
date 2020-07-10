eb99329da11c9533579f8c7e3de972351e296a7f
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-10 10:52:18 -0500

    Auto-commit: Fri Jul 10 2020 10:52:08 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/movii_analisis.sql
+++ superQuery/omar@minka.io/Workbench/movii_analisis.sql
@@ -6,6 +6,8 @@ movii_types AS
         ,ERROR_CODE AS amount
     FROM
         `minka-ach-dw.movii_bridge_log.movii_logs_20_07_09`
+    WHERE 
+        string_field_7="TS"
 )
 ,movii_balance AS 
 (
