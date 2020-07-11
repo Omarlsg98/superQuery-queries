1b2b505e3a1b4bde494b0a80a9e5e6822f85d9c8
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-11 13:12:29 -0500

    Auto-commit: Sat Jul 11 2020 13:12:13 GMT-0500 (hora estándar de Colombia)

---
+++ superQuery/omar@minka.io/Movii_logs/match_a.sql
@@ -0,0 +1,12 @@
+SELECT
+    match.*
+    ,transfer.status AS transfer_status
+    ,transfer.source_bank AS source_bank
+    ,transfer.target_bank AS target_bank
+    ,transfer.created AS transfer_created
+    ,transfer.source_channel AS source_channel
+FROM 
+    minka-ach-dw.movii_bridge_log.movii_action_match_20_07_08 AS match
+LEFT JOIN 
+    minka-ach-dw.ach_tin.transfer
+     ON transfer.transfer_id=match.movii_transfer_id
