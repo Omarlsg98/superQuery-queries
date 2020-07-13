4a302378523534b8f91e0e0de594d0fcf5085a45
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-13 15:53:20 -0500

    1

---
+++ superQuery/omar@minka.io/Signers_address_balance/transfer_balance.sql
@@ -0,0 +1,14 @@
+SELECT
+    transfer.transfer_id
+    ,source_signer
+    ,source.signer AS source
+    ,target_signer
+    ,target.signer AS target
+    ,source.balance AS source_signer_balance
+    ,target.balance AS target_signer_balance
+FROM
+    minka-ach-dw.ach_tin.transfer
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS source ON transfer.source_signer=source.signer
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS target ON transfer.target_signer=target.signer
