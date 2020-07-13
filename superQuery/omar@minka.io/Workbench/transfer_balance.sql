ddb86097578894c6019686431f3488dd3ab9d37b
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-13 15:44:40 -0500

    Auto-commit: Mon Jul 13 2020 15:44:34 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/transfer_balance.sql
+++ superQuery/omar@minka.io/Workbench/transfer_balance.sql
@@ -1,3 +1,21 @@
+SELECT
+    transfer.transfer_id
+    ,source_signer
+    ,source.signer AS source
+    ,target_signer
+    ,target.signer AS target
+    ,source.balance AS source_signer_balance
+    ,target.balance AS target_signer_balance
+FROM
+    minka-ach-dw.ach_tin.transfer
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS source ON transfer.source_signer=source.signer
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS target ON transfer.target_signer=target.signer
+WHERE 
+    transfer_id="0uNXBEXl6yRCdFZnq"
+LIMIT 100;
+
 SELECT * 
 FROM minka-ach-dw.ach_tin.transaction
 WHERE source="wVzSoMesmjhZfz8Jvrdn6ryYuPvPhc7uWA" OR target="wVzSoMesmjhZfz8Jvrdn6ryYuPvPhc7uWA"
