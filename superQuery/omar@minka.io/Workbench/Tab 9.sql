c55ecd3cda9d5f2770be2503233899ccc519dc7a
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-14 16:06:51 -0500

    Auto-commit: Tue Jul 14 2020 16:06:44 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 9.sql
+++ superQuery/omar@minka.io/Workbench/Tab 9.sql
@@ -11,7 +11,7 @@ SELECT
 FROM
     minka-ach-dw.ach_tin.action
 WHERE
-    action_source_bankname IS NOT NULL 
+    action_source_bankrouter IS NOT NULL 
 GROUP BY
     error_code
     ,error_message
