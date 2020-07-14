4a78d31831d6e606d48b604ccb1f39fd81c246fd
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-14 15:21:23 -0500

    Auto-commit: Tue Jul 14 2020 15:21:15 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 9.sql
+++ superQuery/omar@minka.io/Workbench/Tab 9.sql
@@ -1,5 +1,10 @@
 SELECT
     action_source_bankname AS bank
+    ,CASE
+        WHEN action_type="UPLOAD" THEN "debit"
+        WHEN action_type="DOWNLOAD" THEN "credit"
+        WHEN action_type IN ("SEND","REQUEST","REJECT") THEN "action"
+    END AS endpoint_involved
     ,error_code
     ,error_message
     ,count(transfer_id) as number
@@ -12,6 +17,7 @@ GROUP BY
     error_code
     ,error_message
     ,action_source_bankname
+    ,endpoint_involved
 ORDER BY
     bank ASC
     , number DESC
