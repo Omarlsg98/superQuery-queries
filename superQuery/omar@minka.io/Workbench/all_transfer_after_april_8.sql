60b5080e766941b99d6753dcf40cc97d7c6c5eed
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-15 17:09:20 -0500

    Auto-commit: Wed Jul 15 2020 17:09:12 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/all_transfer_after_april_8.sql
+++ superQuery/omar@minka.io/Workbench/all_transfer_after_april_8.sql
@@ -1,10 +1,8 @@
 SELECT
-    status
-    ,COUNT(transfer_id)
+  *
 FROM
     minka-ach-dw.ach_tin.transfer
 WHERE 
     source_channel!='"MassTransferCLI"'
     AND created BETWEEN "2020-04-08" AND "2020-07-09"
-GROUP BY
-    status
+    AND status NOT IN ("COMPLETED","REJECTED")
