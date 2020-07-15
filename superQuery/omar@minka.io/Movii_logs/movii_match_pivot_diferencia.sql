1986067e7a3c4aaeb58bdee87b772f087ea3bd5c
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-15 18:04:09 -0500

    Auto-commit: Wed Jul 15 2020 18:04:02 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_match_pivot_diferencia.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_match_pivot_diferencia.sql
@@ -6,6 +6,9 @@ LEFT JOIN
     minka-ach-dw.ach_tin.transfer
      ON transfer.transfer_id=match.movii_transfer_id
 WHERE
-    UPPER(transfer_id) IN (
-    SELECT transfer_id
-    FROM minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020)
+    UPPER(movii_transfer_id) IN (
+        SELECT transfer_id
+        FROM minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020)
+    OR movii_transfer_id IN (
+        SELECT transfer_id
+        FROM minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020)
