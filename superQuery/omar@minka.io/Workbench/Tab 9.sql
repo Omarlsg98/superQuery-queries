02cdb59ca817bffd0f37424423243d4cb48f3aac
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-15 16:43:37 -0500

    Auto-commit: Wed Jul 15 2020 16:43:21 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 9.sql
+++ superQuery/omar@minka.io/Workbench/Tab 9.sql
@@ -1,22 +1,6 @@
 SELECT
-    action_source_bankrouter AS bank
-    ,CASE
-        WHEN action_type="UPLOAD" THEN "debit"
-        WHEN action_type="DOWNLOAD" THEN "credit"
-        WHEN action_type IN ("SEND","REQUEST","REJECT") THEN "action"
-    END AS endpoint_involved
-    ,error_code
-    ,error_message
-    ,count(transfer_id) as number
+    *
 FROM
-    minka-ach-dw.ach_tin.action
-WHERE
-    action_source_bankrouter IS NOT NULL 
-GROUP BY
-    error_code
-    ,error_message
-    ,action_source_bankrouter
-    ,endpoint_involved
-ORDER BY
-    bank ASC
-    , number DESC
+    minka-ach-dw.ach_tin.transfer
+WHERE 
+    source_channel!="MassTransferCLI"
