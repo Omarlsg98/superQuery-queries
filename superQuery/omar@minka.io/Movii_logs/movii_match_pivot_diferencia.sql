768c41f71e0278eae3945b41360136f095d65378
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-15 18:13:46 -0500

    Auto-commit: Wed Jul 15 2020 18:13:37 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_match_pivot_diferencia.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_match_pivot_diferencia.sql
@@ -1,9 +1,25 @@
 SELECT
-   COUNT(*)
+   match.*
+   ,transfer_type
+   ,transfer_status
+   ,source_bank	
+   ,target_bank
+   ,upload_
+   ,main_action_
+   ,download_target_
+   ,reject_
+   ,download_source_
+   ,download_ambiguous_
+   ,amount
+   ,transfer.created AS transfer_created
+   ,transfer.updated AS transfer_updated
+   ,source_channel
+   ,source_wallet
+   ,target_wallet
 FROM 
     minka-ach-dw.movii_bridge_log.movii_match_20_07_08 AS match
 LEFT JOIN 
-    minka-ach-dw.ach_tin.transfer
+    minka-ach-dw.ach_tin.transfiya_pivot_specific AS transfer
      ON transfer.transfer_id=match.movii_transfer_id
 WHERE
     UPPER(movii_transfer_id) IN (
