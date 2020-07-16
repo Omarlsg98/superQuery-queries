edbbb1ad031064f9c6fb065c31804ba0aec990a0
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-16 16:40:24 -0500

    Auto-commit: Thu Jul 16 2020 16:40:18 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/errors_by_bank.sql
+++ superQuery/omar@minka.io/Workbench/errors_by_bank.sql
@@ -1,26 +1,7 @@
 SELECT
-    action_source_bankrouter AS bank
-    ,CASE
-        WHEN action_type="UPLOAD" THEN "debit"
-        WHEN action_type="DOWNLOAD" THEN "credit"
-        WHEN action_type IN ("SEND","REQUEST","REJECT") THEN "action"
-    END AS endpoint_involved
-    ,error_code
-    ,error_message
-    ,count(transfer_id) as number
+    IF(status="ACCEPTED", target_bank,source_bank) AS bank
+    ,COUNT(transfer_id) as no_continues
 FROM
-    minka-ach-dw.ach_tin.action
-WHERE
-    action_source_bankrouter IS NOT NULL 
-    AND (
-        (action_type="REJECT" AND action_status="ERROR")
-        OR NOT (action_type IN ("SEND","REQUEST") AND action_status="REJECTED" AND action_hash<> 'PENDING')
-    )
-GROUP BY
-    error_code
-    ,error_message
-    ,action_source_bankrouter
-    ,endpoint_involved
-ORDER BY
-    bank ASC
-    , number DESC
+    minka-ach-dw.ach_tin.transfer
+GROUP BY    
+    bank
