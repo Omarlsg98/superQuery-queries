ccaece16c0af5f481ec9abe1e00788ec5507dbec
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-16 17:57:59 -0500

    Auto-commit: Thu Jul 16 2020 17:57:54 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 11.sql
+++ superQuery/omar@minka.io/Workbench/Tab 11.sql
@@ -1,6 +1,11 @@
 SELECT
     transfer_id
+    ,status
+    ,type
+    ,error_code
+    ,error_message
 FROM    
     minka-ach-dw.ach_tin.transfer
 WHERE
     source_bank IS NULL
+    AND status NOT IN ("REJECTED","COMPLETED")
