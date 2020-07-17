101739b943ba3c53ece36126f86037147fd32857
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-17 12:53:55 -0500

    Auto-commit: Fri Jul 17 2020 12:53:50 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/Tab 10.sql
+++ superQuery/omar@minka.io/Workbench/Tab 10.sql
@@ -1,6 +1,6 @@
 SELECT
     * EXCEPT(cellphone,time)
-    , CAST(IF(LENGTH(CAST(time AS STRING))<8, CONCAT("0",time),CAST(time AS STRING)) AS TIME) AS time
+    , IF(LENGTH(CAST(time AS STRING))<8, CONCAT("0",time),CAST(time AS STRING)) AS time
     ,CONCAT("$",cellphone) AS cellphone
 FROM
     minka-ach-dw.movii_bridge_log.avvillas_manual_change
