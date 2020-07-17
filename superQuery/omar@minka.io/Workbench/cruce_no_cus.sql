7247c34dc701d50ff208cf8a204fb3ae146f5c23
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-17 12:02:15 -0500

    Auto-commit: Fri Jul 17 2020 12:02:08 GMT-0500 (hora estándar de Colombia)

---
+++ superQuery/omar@minka.io/Workbench/cruce_no_cus.sql
@@ -0,0 +1,22 @@
+WITH only_avvillas AS(
+SELECT
+    transfer_id
+    ,created
+    ,source_wallet
+    ,status
+    ,t.amount
+FROM
+    minka-ach-dw.ach_tin.transfer AS t
+WHERE 
+    source_bank="Banco AV Villas"
+)
+SELECT
+    transfer_id
+    ,created
+    ,source_wallet
+    ,status
+    ,t.amount
+FROM
+    only_avvillas AS t
+INNER JOIN
+    minka-ach-dw.temp.prueba AS p ON p.date=CAST(SUBSTR(t.created,1,10) AS DATE) AND p.amount=t.amount AND p.cel_origen=t.source_wallet
