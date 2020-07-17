ab53818436349bb6e3316c4e75dd8f871f15784e
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-17 12:45:11 -0500

    Auto-commit: Fri Jul 17 2020 12:45:04 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/cruce_no_cus.sql
+++ superQuery/omar@minka.io/Workbench/cruce_no_cus.sql
@@ -12,17 +12,21 @@ WHERE
     source_bank="Banco AV Villas"
 )
 SELECT
-    transfer_id
-    ,created
+    ARRAY_AGG(
+        STRUCT(
+            transfer_id
+            ,created
+            ,source_bank
+            ,source_wallet
+            ,status
+            ,t.amount
+        ) ) AS transfers 
     ,p.date
     ,p.time
-    ,source_bank
     ,p.cellphone
-    ,source_wallet
-    ,status
-    ,t.amount
     ,_amount_
 FROM
     minka-ach-dw.movii_bridge_log.avvillas_manual_change AS p
 LEFT JOIN
      only_avvillas AS t ON p.date=CAST(SUBSTR(t.created,1,10) AS DATE) AND p._amount_=t.amount AND CONCAT("$",p.cellphone)=t.source_wallet
+GROUP BY date,time,cellphone,_amount_
