a8a7623bd6efdf19de8fc28ab88ff35f7d0bf462
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-18 12:03:54 -0500

    q

--- superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
@@ -33,4 +33,3 @@ LEFT JOIN
     diferencia ON diferencia.transfer_id=match.movii_transfer_id
 WHERE
     match.analisis NOT IN ("  target_OK"," source_OK target_OK"," source_OK"," target_OK")
-LIMIT 100
