baede32fffec3947b2282832aeac343871e7e1d1
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-21 09:25:27 -0500

    Auto-commit: Tue Jul 21 2020 09:25:23 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_Logs/Tab 2.sql
+++ superQuery/omar@minka.io/Transform_Logs/Tab 2.sql
@@ -1,4 +1,9 @@
 SELECT 
-       MAX(timestamp)
+       "transform ids",MAX(timestamp)
 FROM 
     minka-ach-dw.ach_tin_logs.stdout_transfer_ids
+UNION
+SELECT 
+       "stdout",MAX(timestamp)
+FROM 
+    minka-ach-dw.ach_tin_logs.stdout
