1dada4275c5404bb5200cc4d1d17ae7d69fe966d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-21 12:29:34 -0500

    Auto-commit: Tue Jul 21 2020 12:29:20 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_Logs/merge_logs.sql
+++ superQuery/omar@minka.io/Transform_Logs/merge_logs.sql
@@ -1,24 +1,10 @@
-/*CREATE TABLE minka-ach-dw.ach_tin_logs.both_logs AS (
 SELECT 
-   timestamp
-   ,jsonPayload.message as textPayLoad
-FROM 
-    ach-tin-prd.achtin_logs.winston_log
-UNION ALL
-SELECT 
-   timestamp
-   ,textPayLoad
-FROM 
-    ach-tin-prd.achtin_logs.stdout
-)
-*/
-SELECT
-    timestamp
-    ,textPayLoad
-    ,COUNT(*) AS N
-FROM
-    minka-ach-dw.ach_tin_logs.both_logs
-GROUP BY 
-    timestamp,textPayLoad
-HAVING
-    N>1
+    k.timestamp
+    ,k.textPayLoad AS text_pay_load
+FROM (
+      SELECT ARRAY_AGG(row LIMIT 1)[OFFSET(0)] k 
+      FROM 
+        `minka-ach-dw.ach_tin_logs.both_logs` row
+      GROUP BY 
+        timestamp,textPayLoad
+    )
