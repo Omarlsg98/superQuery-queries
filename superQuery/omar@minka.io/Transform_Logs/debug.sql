399847dc409535208c9ad8bde87fef7c072c9cea
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-21 12:36:51 -0500

    Auto-commit: Tue Jul 21 2020 12:36:45 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_Logs/debug.sql
+++ superQuery/omar@minka.io/Transform_Logs/debug.sql
@@ -1,29 +1,7 @@
-WITH 
-# REGEXP to extract the action_id and the transfer_id from stdout logs
-logs AS
-(SELECT 
-       timestamp
-        ,REGEXP_EXTRACT(textPayLoad,'\\b([0-9A-Za-z_]{17})\\b') AS transfer_id
-        ,REGEXP_EXTRACT(textPayLoad,'([0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12})') AS action_id
-       ,textPayLoad
-    FROM 
-        ach-tin-prd.achtin_logs.winston_log
-    WHERE
-        textPayLoad NOT LIKE "%sendActionWithIOU%"
-    )
-,transform AS
-(SELECT
-    timestamp
-    ,IF(action.transfer_id IS NULL, logs.transfer_id, action.transfer_id) AS transfer_id
-    ,logs.action_id
-    ,textPayLoad
-FROM 
-    logs
-LEFT JOIN 
-    minka-ach-dw.ach_tin.action
-        ON action.action_id=logs.action_id)
 SELECT
-    MAX(timestamp)
-FROM 
-    transform
-WHERE transfer_id IS NOT NULL
+    timestamp
+    ,text_pay_load
+FROM
+    minka-ach-dw.ach_tin_logs.both_logs_deduplicated
+WHERE
+    text_pay_load like "%mDNOHvVK7qHlS91zp%"
