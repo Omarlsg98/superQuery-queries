5b43c81378f1f8421cc8229ef5eff64bb89cf23d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-23 09:56:29 -0500

    Auto-commit: Thu Jul 23 2020 09:56:17 GMT-0500 (hora estándar de Colombia)

---
+++ superQuery/omar@minka.io/Workbench/set_ids.sql
@@ -0,0 +1,16 @@
+SELECT
+    omar.action_id AS manual
+    ,julian.action_id AS dataflow
+    , omar.labels.created AS action_created
+    , omar.labels.updated AS action_udpated
+    , julian.labels.created AS jul_action_created
+    , julian.labels.updated AS jul_action_udpated
+FROM 
+    ach-tin-prd-multireg.ach_tin_prod_datastore.action AS omar
+FULL JOIN 
+    ach-tin-prd-multireg.datastore_to_bigquery.action AS julian
+        ON omar.action_id=julian.action_id
+WHERE 
+    omar.action_id IS NULL
+    OR julian.action_id IS NULL
+LIMIT 100
