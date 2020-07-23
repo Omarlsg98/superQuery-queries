ae012bbd82023363bc0ba069e318f9a33cd41aad
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-23 15:49:08 -0500

    Auto-commit: Thu Jul 23 2020 15:49:03 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/add_proper_id.sql
+++ superQuery/omar@minka.io/Movii_logs/add_proper_id.sql
@@ -1,11 +1,4 @@
-CREATE OR REPLACE TABLE  minka-ach-dw.movii_bridge_log.movii_diferencia_14_07_2020 AS (
 SELECT
-    transfer.transfer_id AS transfer_id
-    ,movii.*
+    COUNT (DISTINCT movii_transfer_id)
 FROM 
      minka-ach-dw.movii_bridge_log.movii_diferencia_14_07_2020 AS movii
-LEFT JOIN 
-    minka-ach-dw.ach_tin.transfer ON LOWER(transfer.transfer_id)=LOWER(movii.movii_transfer_id)
-WHERE
-    transfer.transfer_id IS NOT NULL
-)
