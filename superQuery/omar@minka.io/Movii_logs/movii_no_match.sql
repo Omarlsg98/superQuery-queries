e85b5716248fe4c74281545ad3b4880eac9a7ab0
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-23 15:59:38 -0500

    ok2

--- superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
@@ -10,7 +10,7 @@ FROM
     minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020 AS m5
 FULL JOIN 
      minka-ach-dw.movii_bridge_log.movii_diferencia_14_07_2020 AS m14 
-        ON m14.transfer_id = m5.transfer_id 
+        ON LOWER(m14.transfer_id) = LOWER(m5.transfer_id)
 GROUP BY
     transfer_id
 )
