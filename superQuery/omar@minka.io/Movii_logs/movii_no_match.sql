afff30900be981e4332a3c00331d4091b59abca1
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-23 15:44:27 -0500

    ok

--- superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
@@ -1,3 +1,6 @@
+WITH 
+diferencia AS 
+(
 SELECT 
     IF(m14.transfer_id IS NOT NULL, m14.transfer_id,m5.transfer_id) as transfer_id
     ,IF(MAX(m14.transfer_id) IS NOT NULL AND MAX(m5.transfer_id) IS NOT NULL 
@@ -10,3 +13,31 @@ FULL JOIN
         ON m14.transfer_id = m5.transfer_id 
 GROUP BY
     transfer_id
+)
+SELECT
+    match.* EXCEPT(source_channel,updated)
+    ,transfer_type
+    ,upload_
+    ,main_action_
+    ,download_target_
+    ,reject_
+    ,download_source_
+    ,download_ambiguous_
+    ,amount
+    ,transfer.created AS transfer_created
+    ,transfer.updated AS transfer_updated
+    ,transfer.source_channel
+    ,source_wallet
+    ,target_wallet
+    ,IFNULL(diferencia.reported_date,"false") AS in_diferencia
+FROM
+    minka-ach-dw.temp.movii_match AS match
+LEFT JOIN
+    minka-ach-dw.ach_tin.transfiya_pivot_specific AS transfer
+        ON transfer.transfer_id=match.movii_transfer_id
+LEFT JOIN 
+   diferencia ON UPPER(diferencia.transfer_id)=UPPER(match.movii_transfer_id)
+WHERE
+    (match.analisis NOT IN ("  target_OK"," source_OK target_OK"," source_OK"," target_OK","Update movii logs")
+    AND created>"2020-01-01")
+    OR diferencia.transfer_id IS NOT NULL
