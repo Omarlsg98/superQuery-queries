3c2b98de70d1706aee1fac879adcf3a9557eba2d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-29 15:39:54 -0500

    ok

--- superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
@@ -4,7 +4,7 @@ diferencia AS
 SELECT 
     IF(m14.transfer_id IS NOT NULL, LOWER(m14.transfer_id),LOWER(m5.transfer_id)) as transfer_id
     ,IF(MAX(m14.transfer_id) IS NOT NULL AND MAX(m5.transfer_id) IS NOT NULL 
-        ,"14-07 | 05-07"
+        ,"05-07 | 14-07"
         ,IF(MAX(m14.transfer_id) IS NULL,"05-07","14-07") )AS reported_date
 FROM 
     minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020 AS m5
