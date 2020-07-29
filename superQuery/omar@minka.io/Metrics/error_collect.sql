354df4457461ef3a98bb1ed555bea21fe3edf0e5
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-29 10:42:32 -0500

    Auto-commit: Wed Jul 29 2020 10:42:25 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Metrics/error_collect.sql
+++ superQuery/omar@minka.io/Metrics/error_collect.sql
@@ -1,8 +1,4 @@
-INSERT INTO
-    `minka-ach-dw.metrics.errors`(
-    category,
-    number,
-    measured_on)
+
 SELECT
     "apr-jul" AS category ,COUNT(transfer_id) AS number,  CAST(CURRENT_DATETIME("America/Bogota") AS TIMESTAMP) AS measured_on
 FROM
@@ -24,7 +20,9 @@ SELECT
 FROM
     minka-ach-dw.ach_tin.transfer
 WHERE
-    status NOT IN ("REJECTED", "COMPLETED")
+    (status NOT IN ("REJECTED", "COMPLETED")
+        OR (status IN ("PENDING")  AND CAST(SUBSTR(created,1,19) AS DATETIME) < DATETIME_SUB(CURRENT_DATETIME("America/Bogota"),INTERVAL 30 HOUR))
+    )
     AND created > "2020-07-08"
 UNION ALL
 SELECT
