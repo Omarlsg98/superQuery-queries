d777ef7af36514c57d00b3b47520d3e2fc9d6262
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-29 14:35:05 -0500

    Auto-commit: Wed Jul 29 2020 14:34:57 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/search_one.sql
+++ superQuery/omar@minka.io/Movii_logs/search_one.sql
@@ -1,24 +1,7 @@
-WITH 
-    action_balance AS 
-    (
-    SELECT 
-        transfer_id
-        , COUNTIF(action_type="DOWNLOAD") AS downloads
-        , COUNTIF(action_type="UPLOAD") AS uploads
-        , SUM(IF(action_type="DOWNLOAD",-CAST(action_amount AS FLOAT64),CAST(action_amount AS FLOAT64))) AS action_balance
-    FROM
-        minka-ach-dw.ach_tin.action
-    WHERE
-        action_type IN ("DOWNLOAD", "UPLOAD")
-        AND action_hash NOT IN ("PENDING")
-        AND (action_source_signer="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2" 
-            OR action_target_signer="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2")
-    GROUP BY
-        transfer_id
-    )
-SELECT 
+SELECT
     *
-FROM
-    action_balance
+FROM 
+    minka-ach-dw.temp.movii_match
 WHERE
-    transfer_id = "knz9wGw0ttSnrN0us"
+    movii_transfer_id IS NULL
+LIMIT 1000
