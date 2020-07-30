0bd5327cb6ade715e70450d9298e69b337a63aa1
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-30 15:40:19 -0500

    Auto-commit: Thu Jul 30 2020 15:40:11 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -1,10 +1,6 @@
-SELECT CAST(DATE_SUB(CURRENT_DATE("America/Bogota"),INTERVAL 4 DAY) AS STRING) ,  CAST(CURRENT_DATE("America/Bogota") AS STRING);
-
-SELECT CONCAT(TRUE,FALSE);
-
 SELECT
     *
 FROM 
     minka-ach-dw.ach_tin.transaction
 WHERE
-    transaction_id="df386b29014f9f3cbd961ec71567da11a4f5c28127bc98db8a24409e93b3d832"
+    transaction_id LIKE "%df386b29014f9f3cbd961ec71567da11a4f5c28127bc98db8a24409e93b3d832%"
