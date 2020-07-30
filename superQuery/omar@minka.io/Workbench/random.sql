ad40dae38851cb4cd594f4f2381b7d3c2de3bc27
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-07-30 15:54:18 -0500

    Auto-commit: Thu Jul 30 2020 15:54:09 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -1,6 +1,12 @@
 SELECT 
-    *
+    handle
+    ,labels.mobile
+    ,labels.created
+    ,labels.bankName
 FROM 
     minka-ach-dw.ach_tin.signer
 WHERE
-    handle="wYoqKKqis1xq4dXYVFo8JF1KjkED1Gaxf7"
+    handle IN ("wYoqKKqis1xq4dXYVFo8JF1KjkED1Gaxf7",
+                "wWbgfXVtDda9jkHPwY6wgamFEHtCQEBXDK",
+                "wbQ4JP87EZyyc4wfUfLG98Arh1qNZr2DuT",
+                "wdzEtchJiJwfc2pTQbqVGJ8RD9rbJD3CER")
