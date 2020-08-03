132b2fbb8a86e18da1f8c33ef9e119ef83d33e39
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-03 16:10:29 -0500

    Auto-commit: Mon Aug 03 2020 16:10:14 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -1,1 +1,11 @@
-DROP VIEW minka-ach-dw.ach_tin.transfer_action
+SELECT
+    COUNT(action_id)
+FROM 
+    minka-ach-dw.ach_tin.transfer
+INNER JOIN
+    minka-ach-dw.ach_tin.action
+        ON action.transfer_id=transfer.transfer_id
+WHERE
+    action.action_type IN ("REQUEST","SEND")
+    AND (transfer.source_signer!=action.source_signer
+        OR transfer.target_signer!=action.target_signer )
