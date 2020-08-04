61428337cbd3482039e6af09306fa1d61735def3
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-04 15:50:31 -0500

    Auto-commit: Tue Aug 04 2020 15:50:23 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -5,3 +5,4 @@ FROM
 WHERE
     (source_wallet="$573102204561" OR target_wallet="$573102204561")
     AND (source_bank="DAVIPLATA" OR target_bank="DAVIPLATA")
+    AND created BETWEEN   "2020-03-29"  AND "2020-04-08"
