c3946be211a1632d419d56ef3946064959f31669
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-04 15:45:35 -0500

    Auto-commit: Tue Aug 04 2020 15:45:26 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -3,6 +3,6 @@ SELECT
 FROM 
     minka-ach-dw.ach_tin.transfer
 WHERE
-    (source_wallet="$573102204561" OR target_wallet="$573102204561")
-    AND (source_bank="DAVIPLATA" OR target_bank="DAVIPLATA")
+    (source_wallet="$573168059182" OR target_wallet="$573168059182")
+    AND (source_bank="Movii" OR target_bank="Movii")
     AND created BETWEEN "2020-03-07" AND "2020-03-30"
