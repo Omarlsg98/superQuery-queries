93147fba9c59699cc8353cfcddf02c8970888596
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-10 13:56:30 -0500

    Auto-commit: Mon Aug 10 2020 13:56:23 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_merge.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_merge.sql
@@ -1,18 +1,4 @@
-/*Merge new logs of movii with all ones*/
-CREATE OR REPLACE TABLE minka-ach-dw.movii_bridge_log.movii_logs_all AS (
 SELECT
-    IFNULL(CAST(niw.msisdn AS STRING), old.msisdn) AS msisdn
-    , IFNULL(niw.cell_id, old.cell_id) AS cell_id
-    , IFNULL(niw.ftxn_id, old.ftxn_id) AS ftxn_id
-    , IFNULL(niw.service_type, old.service_type) AS service_type
-    , IFNULL(niw.transfer_id, old.transfer_id) AS transfer_id
-    , IFNULL(niw.transfer_on, old.transfer_on) AS transfer_on
-    , IFNULL(niw.value, old.value) AS value
-    , IFNULL(niw.error_code, old.error_code) AS error_code
-    , IFNULL(niw.transfer_status, old.transfer_status) AS transfer_status
+    MAX(transfer_on) as MAX
 FROM 
-    minka-ach-dw.movii_bridge_log.movii_logs_2020_08_10 AS niw
-FULL JOIN
-    minka-ach-dw.movii_bridge_log.movii_logs_all AS old
-        ON LOWER(niw.transfer_id)=LOWER(old.transfer_id)
-)
+    minka-ach-dw.movii_bridge_log.movii_logs_all
