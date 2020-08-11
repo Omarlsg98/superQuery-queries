f758bac7ac58c12323712dae9c019f79e1c681e2
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-11 16:55:58 -0500

    Auto-commit: Tue Aug 11 2020 16:55:48 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -1,7 +1,8 @@
+#Lost IOUs 
 SELECT
     *
 FROM
     minka-ach-dw.ach_tin.transaction
 WHERE
-    iou.data.source="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2" OR iou.data.target="wVCmBRk2jz5fBi47kpzGZezoovzfudv6L2"
+    iou.data.source="wfKKKwyEnVT83AcRMcnjTEdhx68LwWLddS" OR iou.data.target="wfKKKwyEnVT83AcRMcnjTEdhx68LwWLddS"
 LIMIT 100
