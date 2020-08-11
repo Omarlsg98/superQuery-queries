e5d6bd836f9bb52a68baf3ef45247cb2d9dd43f9
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-11 15:03:56 -0500

    asdasd

--- superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_no_match.sql
@@ -1,21 +1,23 @@
 WITH 
-diferencia AS 
+master_summary AS 
 (
 SELECT 
-    IF(m14.transfer_id IS NOT NULL, LOWER(m14.transfer_id),LOWER(m5.transfer_id)) as transfer_id
-    ,IF(MAX(m14.transfer_id) IS NOT NULL AND MAX(m5.transfer_id) IS NOT NULL 
-        ,"05-07 | 14-07"
-        ,IF(MAX(m14.transfer_id) IS NULL,"05-07","14-07") )AS reported_date
-FROM 
-    minka-ach-dw.movii_bridge_log.movii_diferencia_05_07_2020 AS m5
-FULL JOIN 
-     minka-ach-dw.movii_bridge_log.movii_diferencia_14_07_2020 AS m14 
-        ON LOWER(m14.transfer_id) = LOWER(m5.transfer_id)
-GROUP BY
     transfer_id
+   ,IF(fixed_by_minka="pending"
+        ,IF(fixed_source_banks="no-data" OR fixed_target_banks="no-data"
+            ,"no-data"
+            ,IF(minka_analysis="solution-undefined"
+                ,"solution-undefined"
+                ,"pending"
+            )
+        )
+        ,fixed_by_minka) AS master_status
+FROM 
+  minka-ach-dw.temp.master_data
 )
 SELECT
     match.* EXCEPT(source_channel,updated)
+    ,IFNULL(master_status,"not_available") AS deeper_analysis
     ,transfer_type
     ,upload_
     ,main_action_
@@ -29,15 +31,14 @@ SELECT
     ,transfer.source_channel
     ,source_wallet
     ,target_wallet
-    ,IFNULL(diferencia.reported_date,"false") AS in_diferencia
 FROM
     minka-ach-dw.temp.movii_match AS match
 LEFT JOIN
     minka-ach-dw.ach_tin.transfiya_pivot_specific AS transfer
         ON transfer.transfer_id=match.transfer_id
 LEFT JOIN 
-   diferencia ON UPPER(diferencia.transfer_id)=UPPER(match.transfer_id)
+   master_summary AS master 
+        ON LOWER(master.transfer_id)=LOWER(match.transfer_id)
 WHERE
-    (match.analisis NOT IN ("  target_OK"," source_OK target_OK"," source_OK"," target_OK","Update_movii_logs")
-    AND created>"2020-01-01")
-    OR diferencia.transfer_id IS NOT NULL /*Show all tx reported*/
+    match.analisis NOT IN ("  target_OK"," source_OK target_OK"," source_OK"," target_OK","Update_movii_logs")
+    AND created>"2020-01-01"
