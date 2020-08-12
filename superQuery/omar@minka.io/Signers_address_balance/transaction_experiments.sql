a6672664fa8a0e5581a8274a7f70bd0876850194
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-12 16:08:45 -0500

    Auto-commit: Wed Aug 12 2020 16:08:35 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Signers_address_balance/transaction_experiments.sql
+++ superQuery/omar@minka.io/Signers_address_balance/transaction_experiments.sql
@@ -1,10 +1,23 @@
 SELECT
+    /*transaction_id
+    ,iou.data.amount
+    ,iou.data.source
+    ,iou.data.target
+    ,__key__.name
+    ,__has_error__
+    ,created
+    ,iou.hash.value
+    ,action_hash*/
     COUNT(*)
+    ,COUNT(action_id)
+    ,COUNT(transaction_id)
 FROM
     minka-ach-dw.ach_tin.transaction
 FULL JOIN
     minka-ach-dw.temp.action_new_downloads
         ON action_hash=iou.hash.value
 WHERE
-    action_id IS NULL
-    OR transaction_id IS NULL
+    (action_id IS NULL
+        AND transaction.created<"2020-08-12")
+    OR (transaction_id IS NULL
+        AND action_hash IS NOT NULL)
