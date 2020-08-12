a9681cec768a88a8f2566b051bb2f8314543a300
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-12 10:35:09 -0500

    Auto-commit: Wed Aug 12 2020 10:34:52 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Transform_transfer_actions/create action_summary.sql
+++ superQuery/omar@minka.io/Transform_transfer_actions/create action_summary.sql
@@ -1,20 +1,67 @@
-/*ACTION SUMMARY*/ 
-create table  minka-ach-dw.temp.action_summary as
-select transfer_id, min(created) as created, max(updated) as updated , type, sum(count) as count ,ARRAY_AGG(whole_status) as status
-from(
- select transfer_id, type,sum(count) as count,
- STRUCT(
-      status,
-      STRUCT(sum(count) as total,sum(with_hash) as with_hash,sum(without_hash)as without_hash) as count,
-      max(updated)as updated,
-      ARRAY_AGG(STRUCT(code,message,STRUCT(count as total,with_hash,without_hash) as count))as error
-    )as whole_status,
- min(created) as created, max(updated) as updated
- FROM(
-    select transfer_id, type,action_status as status, error_code as code,error_message as message, count(*) as count,
-    sum(IF(action_hash<> 'PENDING',1,0)) as with_hash, sum(IF(action_hash='PENDING',1,0)) as without_hash,
-    min(action_created) as created, max(action_udpated) as updated
-    From minka-ach-dw.temp.action_new_downloads
-    group by transfer_id,type,action_status,error_code,error_message)as T1
-  group by transfer_id,type,status) as T2
-group by transfer_id, type
+/*ACTION SUMMARY*/
+WITH
+grouped_by_error AS
+(
+    SELECT
+      transfer_id,
+      type,
+      action_status AS status,
+      error_code AS code,
+      error_message AS message,
+      COUNT(*) AS count,
+      SUM(IF(action_hash <> 'PENDING', 1, 0)) AS with_hash,
+      SUM(IF(action_hash = 'PENDING', 1, 0)) AS without_hash,
+      MIN(action_created) AS created,
+      MAX(action_udpated) AS updated
+    FROM
+      minka-ach-dw.temp.action_new_downloads
+    GROUP BY
+      transfer_id,type,action_status, error_code, error_message
+),
+grouped_by_status AS
+(
+    SELECT
+      transfer_id,
+      type,
+      SUM(count) AS count,
+      SUM(with_hash) AS with_hash,
+      STRUCT(
+        status,
+        STRUCT(
+          SUM(count) AS total,
+          SUM(with_hash) AS with_hash,
+          SUM(without_hash) AS without_hash
+        ) AS count,
+        MAX(updated) AS updated,
+        ARRAY_AGG(
+          STRUCT(
+            code,
+            message,
+            STRUCT(
+                count AS total,
+                with_hash,
+                without_hash
+            ) AS count
+          )
+        ) AS error
+      ) AS whole_status,
+      MIN(created) AS created,
+      MAX(updated) AS updated
+    FROM
+        grouped_by_error
+    GROUP BY
+      transfer_id,type,status
+)
+SELECT
+  transfer_id,
+  MIN(created) AS created,
+  MAX(updated) AS updated,
+  type,
+  SUM(count) AS count,
+  SUM(with_hash) AS with_hash,
+  ARRAY_AGG(whole_status) AS status
+FROM
+    grouped_by_status
+GROUP BY
+  transfer_id, type
+LIMIT 100
