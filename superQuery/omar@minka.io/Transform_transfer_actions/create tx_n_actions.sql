8d1a59f9697c83f3c0e2a7016d60a957f5b995ba
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-12 10:58:37 -0500

    trying_nulls

--- superQuery/omar@minka.io/Transform_transfer_actions/create tx_n_actions.sql
+++ superQuery/omar@minka.io/Transform_transfer_actions/create tx_n_actions.sql
@@ -1,22 +1,73 @@
 /*TX N ACTIONS*/
-create table minka-ach-dw.temp.tx_n_actions as
-select * EXCEPT(error_code,error_message), STRUCT(error_code as code,error_message as message) as error,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and (asum.type="SEND" or asum.type="REQUEST")) as main_action,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and asum.type="UPLOAD") as upload,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and asum.type="DOWNLOAD_TARGET") as download_target,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and asum.type="REJECT") as reject,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and asum.type="DOWNLOAD_SOURCE") as download_source,
-  (select STRUCT(created, updated, count, status) 
-  from minka-ach-dw.temp.action_summary asum
-  where t.transfer_id=asum.transfer_id and asum.type="DOWNLOAD_AMBIGUOUS") as download_ambiguous
-from minka-ach-dw.ach_tin.transfer t
+WITH 
+tx_n_actions_nulls AS (
+SELECT
+  * EXCEPT(error_code, error_message),
+  STRUCT(error_code AS code, error_message AS message) AS error,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND (
+        asum.type = "SEND"
+        or asum.type = "REQUEST"
+      )
+  ) AS main_action,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND asum.type = "UPLOAD"
+  ) AS upload,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND asum.type = "DOWNLOAD_TARGET"
+  ) AS download_target,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND asum.type = "REJECT"
+  ) AS reject,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND asum.type = "DOWNLOAD_SOURCE"
+  ) AS download_source,
+  (
+    SELECT
+      STRUCT(created, updated, count,with_hash, status)
+    FROM
+      minka-ach-dw.temp.action_summary asum
+    WHERE
+      t.transfer_id = asum.transfer_id
+      AND asum.type = "DOWNLOAD_AMBIGUOUS"
+  ) AS download_ambiguous 
+FROM
+  minka-ach-dw.ach_tin.transfer t
+)
+SELECT
+    * EXCEPT(main_action,download_target,download_source,download_ambiguous,reject,upload)
+    ,STRUCT(main_action.* EXCEPT(count,with_hash)
+        ,count
+        ,with_hash) AS main_action
+FROM
+    tx_n_actions_nulls
