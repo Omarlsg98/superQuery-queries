68c09d6ae28f09401be002ffee1a83f1405e9be7
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-12 19:33:48 -0500

    Auto-commit: Wed Aug 12 2020 19:33:39 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Board 10/Tab 3.sql
+++ superQuery/omar@minka.io/Board 10/Tab 3.sql
@@ -1,14 +1,6 @@
 SELECT 
-   action_type
-   ,action_status
-   ,COUNT(action_id) AS conteo
+   COUNT(action_id) AS conteo
    ,MAX(action_created) AS fecha_maxima_registra
    ,MIN(action_created) AS fecha_minima
 FROM
     minka-ach-dw.ach_tin.action
-GROUP BY
-    action_type
-    ,action_status
-ORDER BY 
-    action_type DESC
-    ,conteo DESC
