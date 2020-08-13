bf9b9cc92c4918744bbfb58ff3f9c8581bb611d7
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-13 12:32:14 -0500

    V2

---
+++ superQuery/omar@minka.io/Scenario_finder/pivot_balance_specific_v2.sql
@@ -0,0 +1,55 @@
+SELECT 
+    transfer_id 
+    ,type AS transfer_type
+    ,status AS transfer_status
+    ,(SELECT
+        String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM 
+        UNNEST(upload.status)) as upload_
+    ,(SELECT
+        String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM
+        UNNEST(main_action.status)) as main_action_
+    ,(SELECT  
+        String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM
+        UNNEST(download_target.status)) as download_target_
+    ,(SELECT 
+       String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM
+        UNNEST(reject.status)) as reject_ 
+    ,(SELECT 
+        String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM 
+        UNNEST(download_source.status)) as download_source_
+    ,(SELECT 
+       String_AGG(CONCAT(status," ",count.total, "/",count.with_hash), " || ")
+    FROM 
+        UNNEST(download_ambiguous.status)) as download_ambiguous_
+    ,IF(source.balance>=amount,TRUE,FALSE) AS source_has_balance
+    ,IF(target.balance>=amount,TRUE,FALSE) AS target_has_balance
+    ,source_bank
+    ,target_bank
+    ,created
+    ,updated
+    ,source_wallet
+    ,target_wallet
+    ,amount
+    ,source.balance AS source_balance
+    ,target.balance AS target_balance
+    ,source_signer
+    ,target_signer
+    ,source_channel
+    ,error
+    ,upload.with_hash AS uploads_signed
+    ,main_action.with_hash AS main_actions_signed
+    ,download_target.with_hash AS downs_target_signed
+    ,reject.with_hash AS rejects_signed
+    ,download_source.with_hash AS downs_source_signed
+    ,download_ambiguous.with_hash AS downs_ambiguous_signed
+FROM
+    minka-ach-dw.temp.tx_n_actions 
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS source ON source_signer=source.signer
+LEFT JOIN 
+    minka-ach-dw.ach_tin.signer_balance AS target ON target_signer=target.signer
