3f86f0644d7426d4e33c690f621dfe02b3f6a36e
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-13 12:39:37 -0500

    Auto-commit: Thu Aug 13 2020 12:39:27 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Scenario_finder/bad_completed.sql
+++ superQuery/omar@minka.io/Scenario_finder/bad_completed.sql
@@ -3,9 +3,19 @@ SELECT
 FROM
     `minka-ach-dw.ach_tin.pivot_balance_specific_v2` as t
 WHERE
+    /*Completed sin download en target...*/
     (
-        downs_target_signed=0
-        AND downs_ambiguous_signed=0
+        (
+            downs_target_signed=0
+            AND downs_ambiguous_signed=0
+            AND transfer_status="COMPLETED"
+        )
+        OR
+         (
+            uploads_signed>0
+            AND downs_source_signed=0 
+            AND downs_ambiguous_signed=0 
+            AND transfer_status="REJECTED"
+        )
     )
-    AND transfer_status="COMPLETED"
     AND source_channel!='"MassTransferCLI"'
