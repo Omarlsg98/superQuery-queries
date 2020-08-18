782c6ce7563dfa7a2397c0a73ce2946d7b6d664e
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-18 17:14:17 -0500

    Auto-commit: Tue Aug 18 2020 17:14:06 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/random.sql
+++ superQuery/omar@minka.io/Workbench/random.sql
@@ -1,6 +1,7 @@
 SELECT
-   *
+    *
 FROM
-    minka-ach-dw.ach_tin.action
+    minka-ach-dw.ach_tin.transaction
 WHERE
-    action_type IN ("WITHDRAW","TOPUP")
+   transaction_id="1e29680791952f230fef59c509b58b2c042fe86888b5de0c9fc6ae5544122aec"
+LIMIT 100
