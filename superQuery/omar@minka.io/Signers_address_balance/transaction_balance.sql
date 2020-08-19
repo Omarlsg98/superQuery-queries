a7a465d6de73bd1053491a38ebaf25c1a20d441d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 14:39:31 -0500

    Auto-commit: Wed Aug 19 2020 14:39:16 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Signers_address_balance/transaction_balance.sql
+++ superQuery/omar@minka.io/Signers_address_balance/transaction_balance.sql
@@ -1,6 +1,26 @@
-SELECT
-   SUM(CAST(iou.data.amount AS FLOAT64)) 
-FROM
+#Get the last movements by signer and position (source/target)
+#wRxZte3mwBmUbyKBFKMUQc91xZYzXXE4f9
+WITH summary AS (
+  SELECT
+    iou.data.source AS signer
+    ,- SUM(CAST(iou.data.amount AS FLOAT64)) AS amount
+    ,"expend" AS type
+  FROM
     `minka-ach-dw.ach_tin.transaction` AS a
-WHERE
-    a.iou.data.source="wRxZte3mwBmUbyKBFKMUQc91xZYzXXE4f9"
+  GROUP BY
+    iou.data.source
+  UNION ALL
+  SELECT
+    iou.data.target AS signer,
+    SUM(CAST(iou.data.amount AS FLOAT64)) AS amount
+    ,"income" AS type
+  FROM
+    `minka-ach-dw.ach_tin.transaction` AS a
+  GROUP BY
+    iou.data.target
+)
+SELECT
+    COUNT(signer)
+    
+FROM
+    summary
