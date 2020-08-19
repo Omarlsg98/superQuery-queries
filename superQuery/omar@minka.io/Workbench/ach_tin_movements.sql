65ec2235ebf9744c6d0e2b788c23d6a7a0683317
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 11:17:46 -0500

    Auto-commit: Wed Aug 19 2020 11:17:36 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Workbench/ach_tin_movements.sql
+++ superQuery/omar@minka.io/Workbench/ach_tin_movements.sql
@@ -6,5 +6,4 @@ WHERE
     action_target_signer IN ( "wd7VoAD3PzRdRRuKUbSUzL2gFgSD4Z8HRC", #TIN signer
                             "wXmZi7xP4x2kyTjXH55EyWQSczvrLRfHL5") #ACH signer
     OR
-    action_source_signer IN ( "wd7VoAD3PzRdRRuKUbSUzL2gFgSD4Z8HRC", #TIN signer
-                            "wXmZi7xP4x2kyTjXH55EyWQSczvrLRfHL5") #ACH signer
+    action_source_signer IN ( "wd7VoAD3PzRdRRuKUbSUzL2gFgSD4Z8HRC")
