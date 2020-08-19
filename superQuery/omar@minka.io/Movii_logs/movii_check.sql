8834acc39c147f70d780ebd332bb6dafb37d8d6b
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 11:46:02 -0500

    Auto-commit: Wed Aug 19 2020 11:45:52 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_check.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_check.sql
@@ -1,6 +1,24 @@
+#Check duplicates
 SELECT
-    *
+    transfer_id
+    ,COUNT(transfer_id) AS conteo
 FROM
+    movii_bridge_log.movii_logs_all
+GROUP BY
+    transfer_id
+HAVING 
+    conteo>1;
+
+
+#Check max dates of the movii_logs
+SELECT
+    MAX(transfer_on) as MAX, "logs_all"
+FROM 
     minka-ach-dw.movii_bridge_log.movii_logs_all
-WHERE 
-    LOWER(transfer_id) =LOWER("MP200413.1920.E00377")
+UNION ALL
+SELECT
+    MAX(created) as MAX , "transform"
+FROM 
+    minka-ach-dw.movii_bridge_log.movii_logs_transform;
+    
+    
