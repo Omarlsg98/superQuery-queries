f02eb5dd761942bdf39124cb48838bd5c0f46280
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 11:35:52 -0500

    Auto-commit: Wed Aug 19 2020 11:35:43 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_manual.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_manual.sql
@@ -1,11 +1,4 @@
-#Add cell_ids to movii_manual
-CREATE OR REPLACE TABLE minka-ach-dw.movii_bridge_log.movii_manual_ids AS (
-    SELECT
-        manual.* 
-        ,IFNULL(manual.related_cus,normal.cell_id) AS cell_id
-    FROM
-        minka-ach-dw.movii_bridge_log.movii_manual_adjustments AS manual
-    LEFT JOIN
-        minka-ach-dw.movii_bridge_log.movii_logs_all AS normal
-            ON LOWER(normal.transfer_id)=LOWER(manual.related_transfer_id)
-)
+SELECT 
+    *
+FROM 
+    minka-ach-dw.movii_bridge_log.movii_manual_ids
