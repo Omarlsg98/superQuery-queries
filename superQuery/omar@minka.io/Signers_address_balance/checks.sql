5eede13434b068684b0381f9a70947f4746c82ea
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 16:00:56 -0500

    Auto-commit: Wed Aug 19 2020 16:00:47 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Signers_address_balance/checks.sql
+++ superQuery/omar@minka.io/Signers_address_balance/checks.sql
@@ -1,6 +1,9 @@
 SELECT
-    COUNT(signer)
+    signer
+    ,balance AS calc_balance
 FROM
-    minka-ach-dw.tests.transaction_balance
+    `minka-ach-dw.tests.transaction_balance`
 WHERE
-    balance>0
+    (balance>0 AND RAND()<=0.8) #9000
+    OR
+    (balance=0 AND RAND()<=0.01); #260000
