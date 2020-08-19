6aa7f624efc0636d939329c085c939808475359f
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 15:56:21 -0500

    Auto-commit: Wed Aug 19 2020 15:56:11 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Signers_address_balance/checks.sql
+++ superQuery/omar@minka.io/Signers_address_balance/checks.sql
@@ -1,7 +1,1 @@
-SELECT
-   *
-FROM 
-    `minka-ach-dw.ach_tin.transaction`
- WHERE
-    #iou.data.symbol="wd7VoAD3PzRdRRuKUbSUzL2gFgSD4Z8HRC"
-    transaction_id="d673082411a25d59b54cb117dcbd73a47be20259d19578d426f87600564122ec"
+SELECT RAND()
