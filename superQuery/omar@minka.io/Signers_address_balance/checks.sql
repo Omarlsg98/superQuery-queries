56f113f1372606256862e4183adcbc27d7842b1a
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 15:05:22 -0500

    Auto-commit: Wed Aug 19 2020 15:05:11 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Signers_address_balance/checks.sql
+++ superQuery/omar@minka.io/Signers_address_balance/checks.sql
@@ -1,7 +1,27 @@
 #Some searchs
+/*
 SELECT
    iou.data.symbol
 FROM 
     `minka-ach-dw.ach_tin.transaction`
 GROUP BY
     iou.data.symbol
+;
+*/
+
+#check 1: total balance 
+#it must be 0
+SELECT
+    SUM(balance) AS total
+FROM
+    `minka-ach-dw.tests.transaction_balance`;
+
+#check 2: signers with negative balance
+#It must only be one 
+SELECT
+    signer
+    ,balance
+FROM
+    `minka-ach-dw.tests.transaction_balance`  
+WHERE
+    balance<0;
