36b83895e15c8b4e52f95b1f24589465b7a4489d
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-08-19 11:57:59 -0500

    Auto-commit: Wed Aug 19 2020 11:57:50 GMT-0500 (hora estándar de Colombia)

--- superQuery/omar@minka.io/Movii_logs/movii_check.sql
+++ superQuery/omar@minka.io/Movii_logs/movii_check.sql
@@ -3,4 +3,7 @@ SELECT
 FROM
     minka-ach-dw.movii_bridge_log.movii_logs_all
 WHERE 
-    LOWER(transfer_id) =LOWER("CI200316.1725.A01734")
+    transfer_id IN ("CI200316.1725.A01734",
+"CI200512.1450.A01593",
+"CI200513.1435.A02364",
+"CI200528.1940.A00842")
