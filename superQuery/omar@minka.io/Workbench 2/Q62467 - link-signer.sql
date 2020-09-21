183d7c0797aa81b0d5074afe4c08000c04cd9c3a
Omar Leonardo Sanchez Granados <omarlsg98@gmail.com>
2020-09-21 15:42:53 -0500

    2

--- superQuery/omar@minka.io/Workbench 2/Q62467 - link-signer.sql
+++ superQuery/omar@minka.io/Workbench 2/Q62467 - link-signer.sql
@@ -36,4 +36,44 @@ LEFT JOIN
 LEFT JOIN
   unnest_wallet AS wallet ON wallet.u_signer=link.target
 WHERE
+  signer.labels.routerReference="$bancocajasocial";
+/*  
+Cordial saludo equipo Minka
+Agradezco su apoyo entregando un reporte de relaciones de confianza que contenga los siguientes campos:
+link_id	
+created	
+source	(Wallet)
+target	(signer)
+target_router_reference	
+target_wallet
+identification (target_signer)
+Filtros:
+Desde edl inicio hasta el 11 de sepriembre
+target_router_reference = $bancocajasocial */
+WITH unnest_wallet AS 
+(
+  SELECT
+    u_signer
+    ,handle 
+  FROM
+    `minka-ach-dw.ach_tin.wallet` AS wallet
+  CROSS JOIN UNNEST(wallet.signer) AS u_signer
+)
+ 
+SELECT
+  link_id,
+  created,
+  source,  
+  target,
+  signer.labels.routerReference AS target_router_reference,
+  wallet.handle AS target_wallet,
+  labels.identification AS target_identification
+FROM
+  `minka-ach-dw.ach_tin.link` AS link
+LEFT JOIN
+  `minka-ach-dw.ach_tin.signer` AS signer ON signer.handle=link.target
+LEFT JOIN
+  unnest_wallet AS wallet ON wallet.u_signer=link.target
+WHERE
   signer.labels.routerReference="$bancocajasocial"
+  AND link.created BETWEEN "2019-09-13" AND "2020-09-11";
